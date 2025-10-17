8e6d9fae76d7368cb4c04566637c14176768826a
bye <wave.foliage@gmail.com>
2025-10-17 15:48:31 +0800

    commit

--- R/deseq2_demo.R
+++ R/deseq2_demo.R
@@ -29,11 +29,9 @@ deseq2_demo<-function(exp, group, compared,merge=F, p.name = "pvalue", symbol=NU
   compared<-as.vector(str_split(compared,"-",simplify = T))%>%str_trim()
 
   dds <- DESeqDataSetFromMatrix(exp, data.frame(group), design= ~ group )
-  dds <- estimateSizeFactors(dds)
-  dds <- estimateDispersions(dds)
   dds <- DESeq(dds)
   #x <- results(dds,contrast = c("group",compared[[1]],compared[[2]]))%>%as.data.frame()
-  x<-lfcShrink(dds = dds,contrast = c("group",compared[[1]],compared[[2]]),type = "apeglm")
+  x<-lfcShrink(dds = dds,contrast = resultsNames(dds)[2],type = "apeglm")
   count<-counts(dds,normalized=T)
   x<-as.data.frame(x)
   x$sig[(x[, p.name] > p.value | x[, p.name] == "NA") | (x[, fc.name] < fc.value) & x[, fc.name] > -fc.value] <- "Stable"
@@ -50,6 +48,6 @@ deseq2_demo<-function(exp, group, compared,merge=F, p.name = "pvalue", symbol=NU
     write.table(data.frame(Symbol=rownames(x),x,exp),file = file.name,sep = "\t",quote = F,row.names = F,col.names = T)
   }
 
-  return(x)
+  return(list(x,dds))
 }
 
